5b2b0df0f9971b4a7e579000740a2e7995e4efa7
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-09-11 22:10:27 +0530

    Add files via upload

---
+++ matplot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0,6])
+ypoints = np.array([0,200])
+plt.plot(xpoints,ypoints)
+plt.show()
+
+#another one
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,8])
+y = np.array([3,10])
+plt.plot(x,y)
+plt.show()
+
+#Another one
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,8])
+y = np.array([3,10])
+plt.plot(x,y,'o')
+plt.show()
